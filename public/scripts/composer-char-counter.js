d21db2ac5a1b0e1ab3a2cc73245eed09471cfdd7
Dan Pappo <dpappo@gmail.com>
2021-02-22 17:25:54 -0500

    implement character count feature

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#tweet-text').on('keyup',(event) => {
-    //TODO discuss with classmates what can go into selector instead of counter and tweet-text
+  //TODO discuss with classmates what can go into selector instead of counter and tweet-text
     $('.counter').text(140 - $('#tweet-text').val().length);
     if (Number($('.counter').text()) < 0) {
       $('.counter').css('color', 'red');
