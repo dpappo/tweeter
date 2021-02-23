012107e82dcf69ac62522bab4228931eee4c70ab
Dan Pappo <dpappo@gmail.com>
2021-02-23 18:37:49 -0500

    new styling and new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,6 +29,32 @@ let renderTweet = function(tweet) {
 };
 
 const tweetData = [
+  {"user": {
+    "name": "wint",
+    "avatars": "https://pbs.twimg.com/profile_images/847818629840228354/VXyQHfn0_400x400.jpg",
+    "handle": "@dril"
+  },
+  "content": {
+    "text": "i shou;ld be allowed on the roof of any building"
+  },
+  "created_at": "2:38 PM · Nov 15, 2020"},
+  {"user": {
+    "name": "ye 🌍",
+    "avatars": "https://pbs.twimg.com/profile_images/1353936799484301312/fyN3pz3__400x400.jpg",
+    "handle": "@yedoye_"
+  },
+  "content": {
+    "text": "every day I wake up and begin the 16 hour process of getting ready for bed"
+  },
+  "created_at": "3:41 PM · Feb 2, 2021"},{"user": {
+    "name": "Christina",
+    "avatars": "https://pbs.twimg.com/profile_images/1282382759118483459/aK0lRqto_400x400.jpg",
+    "handle": "@floozyesq"
+  },
+  "content": {
+    "text": "me: it’s okay to be a person struggling with productivity during a prolonged crisis \nmy brain: not u though \nme: not me though"
+  },
+  "created_at": "12:06 PM · Oct 7, 2020"},
   {
     "user": {
       "name": "Newton",
@@ -50,7 +76,8 @@ const tweetData = [
       "text": "Je pense , donc je suis"
     },
     "created_at": 1461113959088
-  }
+  },
+  
 ];
 
 createTweetElement(tweetData);
