9e62d61762134c08db1c101db90eaf9a4be5af9d
Dan Pappo <dpappo@gmail.com>
2021-02-23 16:40:31 -0500

    dynamically render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,59 +4,54 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// let renderArticlesHTML = function(obj) {
-// for (let tweet in obj) {
-// $('main.container').append(createTweetElement(obj));
-// }
-// };
+let createTweetElement = function(obj) {
+  for (let tweet of obj) {
+    $('main.container').append(renderTweet(tweet));
+  }
+};
 
-// let createTweetElement = function(tweet) {
-//   return `<section class="tweet-container">
-//   <article class="tweet">
-//     <header class="tweet-header">
-//       <div class="fullName">
-//         <div class="icon">${tweet.user.avatars}</div>
-//         ${tweet.user.name}
-//       </div>
-//       <div class="userName">${tweet.user.handle}</div>
-//     </header>
-//     <div class="tweet">${tweet.content.tweet}</div>
-//     <footer class="tweet-footer">
-//       <div class="datePosted">${tweet.created_at}</div>
-//       <div class="tweeterIcons">🚩 🔀 ❤️</div>
-//     </footer>
-//   </article>`;
-// };
+let renderTweet = function(tweet) {
+  return `<section class="tweet-container">
+  <article class="tweet">
+    <header class="tweet-header">
+      <div class="fullName">
+        <div class="icon"><img src="${tweet.user.avatars}"></div>
+        ${tweet.user.name}
+      </div>
+      <div class="userName">${tweet.user.handle}</div>
+    </header>
+    <div class="tweet">${tweet.content.text}</div>
+    <footer class="tweet-footer">
+      <div class="datePosted">${tweet.created_at}</div>
+      <div class="tweeterIcons">🚩 🔀 ❤️</div>
+    </footer>
+  </article>`;
+};
 
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// };
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
+createTweetElement(tweetData);
 
-// $('main.container').append(`<section class="tweet-container">
-// <article class="tweet">
-//   <header class="tweet-header">
-//     <div class="fullName">
-//       <div class="icon">${tweetData.user.avatars}</div>
-//       ${tweetData.user.name}
-//     </div>
-//     <div class="userName">${tweetData.user.handle}</div>
-//   </header>
-//   <div class="tweet">${tweetData.content.tweet}</div>
-//   <footer class="tweet-footer">
-//     <div class="datePosted">${tweetData.created_at}</div>
-//     <div class="tweeterIcons">🚩 🔀 ❤️</div>
-//   </footer>
-// </article>`);
-
-// renderArticlesHTML(tweetData);
-
-let VariableWithUniqueName = "";
