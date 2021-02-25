57c346d9288cc08ab5a6fbea6934b19b473615e3
Dan Pappo <dpappo@gmail.com>
2021-02-25 15:52:21 -0500

    refactor

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,15 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-let createTweetElement = function(array) {
-  for (let i = 0; i < array.length; i++) {
-    $('#tweets-section').prepend(renderTweet(array[i]));
+// Helper functions:
+
+let addTweetElement = function(tweets) {
+  for (let i = 0; i < tweets.length; i++) {
+    $('#tweets-section').prepend(renderTweet(tweets[i]));
   }
 };
 
 let renderTweet = function(tweet) {
-  return `
-  <article class="tweet">
+  return `<article class="tweet">
     <header class="tweet-header">
       <div class="fullName">
         <div class="icon"><img src="${tweet.user.avatars}"></div>
@@ -28,28 +29,6 @@ let renderTweet = function(tweet) {
   </article>`;
 };
 
-let newTweet = function(tweet) {
-  return `
-  <article class="tweet">
-    <header class="tweet-header">
-      <div class="fullName">
-        <div class="icon"><img src="./images/circle-cropped.png"></div>
-        <p class="username">Dan Pappo</p>
-      </div>
-      <div class="userName">dpappo</div>
-    </header>
-    <div class="tweet">${tweet}</div>
-    <footer class="tweet-footer">
-      <div class="datePosted">${moment(new Date)}</div>
-      <div class="tweeterIcons">🚩 🔀 ❤️</div>
-    </footer>
-  </article>`;
-};
-
-let submitNewTweet = function(tweet) {
-  $(newTweet(tweet)).prependTo('#tweets-section');
-};
-
 // don't let users insert scripts
 const escape =  function(str) {
   let div = document.createElement('div');
@@ -57,73 +36,72 @@ const escape =  function(str) {
   return div.innerHTML;
 };
 
+const loadTweet = function(handleData) {
+  $.ajax({method: 'GET',
+    url: '../tweets',
+    dataType: "json"
+  }).done(function(data) {
+    $("#tweets-section").empty();
+    handleData(data);
+  });
+};
+
+const navScrollListener = $(".nav-prompt").click(() => {
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $("section.new-tweet").offset().top - 125
+  }, 1000);
+  $('textarea').focus();
+});
+
+const errorHandler = function(errorMsg) {
+  $('.validationError').text(`${errorMsg}`).slideDown();
+  setTimeout(function() {
+    $('.validationError').slideUp().empty();
+  }, 3000);
+};
+
+//tweet submission handler
+const handleSubmit = $('#tweet-form').submit((event) => {
+  event.preventDefault();
+  const tweetData = escape($('#tweet-text').val());
+
+  // empty tweet error
+  if (tweetData === "") {
+    errorHandler("Not enough tweet to tweet. Try adding some content ✍️");
+
+    // tweet > 140 char
+  } else if (tweetData.length > 140) {
+    errorHandler("Too long, too long!! Try again with less than 140 characters 📝");
+
+    //post case
+  } else if (tweetData !== "") {
+    $.ajax({method: 'POST',
+      url: '/tweets',
+      data: {text: tweetData}})
+      .done(() => {
+        loadTweet(addTweetElement);
+      })
+      .then(() => {
+        $("#tweet-text").val("");
+        $("output.counter").text("140");
+      });
+  }
+});
+
+const focusBorder = $("#tweet-text").on("focus", function() {
+  $("section.new-tweet").css("box-shadow", "inset 0 0 0 5px #4056A1");
+});
+const blurBorder = $("#tweet-text").on("blur", function() {
+  $("section.new-tweet").css("box-shadow", "none");
+});
+
+// jQuery on ready:
 $(document).ready(function() {
 
-  // load tweets from db
-  const loadTweet = function(handleData) {
-    $.ajax({method: 'GET',
-      url: '../tweets',
-      dataType: "json"
-    }).done(function(data) {
-      $("#tweets-section").empty();
-      handleData(data);
-    });
-  };
-
-  loadTweet(createTweetElement);
-
-
-  //scroll from nav to new tweet with focus
-  $(".nav-prompt").click(() => {
-    $([document.documentElement, document.body]).animate({
-      scrollTop: $("section.new-tweet").offset().top - 125
-    }, 1000);
-    $('textarea').focus();
-  });
-
-  //submit tweet handler
-  $('#tweet-form').submit((event) => {
-    event.preventDefault();
-    const tweetData = escape($('#tweet-text').val());
-
-    // empty tweet error
-    if (tweetData === "") {
-      $('.validationError').text("Not enough tweet to tweet. Try adding some content ✍️").slideDown();
-      setTimeout(function() {
-        $('.validationError').empty().slideUp();
-      }, 3000);
-
-      // tweet > 140 char
-    } else if (tweetData.length > 140) {
-      $('.validationError').text("Too long, too long!! Try again with less than 140 characters 📝").slideDown();
-      setTimeout(function() {
-        $('.validationError').empty();
-        $('.validationError').slideUp();
-      }, 3000);
-
-      //post case
-    } else if (tweetData !== "") {
-      $.ajax({method: 'POST',
-        url: '/tweets',
-        data: {text: tweetData}})
-        .done(() => {
-          loadTweet(createTweetElement);
-        })
-        .then(() => {
-          $("#tweet-text").val("");
-          $("output.counter").text("140");
-        });
-    }
-  });
-
-
-
-  //hover and focus border for new tweet section
-  $("#tweet-text").on("focus", function() {
-    $("section.new-tweet").css("box-shadow", "inset 0 0 0 5px #4056A1");
-  });
-  $("#tweet-text").on("blur", function() {
-    $("section.new-tweet").css("box-shadow", "none");
-  });
+  loadTweet(addTweetElement);
+  navScrollListener;
+  handleSubmit;
+  focusBorder;
+  blurBorder;
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
-  $('#tweet-text').on('keyup',(event) => {
-  //TODO discuss with classmates what can go into selector instead of counter and tweet-text
+  $('#tweet-text').on('keyup',() => {
+    //TODO discuss with classmates what can go into selector instead of counter and tweet-text
     $('.counter').text(140 - $('#tweet-text').val().length);
     if (Number($('.counter').text()) < 0) {
-      // not wrong, but, jQuery add and remove Class is preferred
-      // $('.counter').css('color', 'red');
       $('.counter').addClass('below-140');
-      
     } else {
       $('.counter').removeClass('below-140');
-      // $('.counter').css('color', '#55514b');
     }
   });
 });
